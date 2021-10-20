690004b6e94d5d4f56e00f33877d61757e69f283
janjarozek <74878517+janjarozek@users.noreply.github.com>
2021-10-20 22:02:22 +0200

    first commit - hello world

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import '../../_sass/styles.scss'
+export default function Header() {
+    return (
+        <h2 className="Header">
+            .:: react-redux user-app ::.
+        </h2>
+    )
+}
